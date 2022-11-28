bd1ee904474324fdc31a8729ac11227ad16feb72
Md Mahbub Mosharrof <106234160+MARUF53@users.noreply.github.com>
2022-11-28 22:12:53 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$('.dest_slider').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    centerPadding: "0px",
+  });
+
+
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
